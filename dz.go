710e42a6db47b50ec810f4234898cd321de80415
konygin73 <62308189+konygin73@users.noreply.github.com>
2025-08-10 18:36:00 +0300

    дз 8 завершение по сигналу

--- dz.go
+++ dz.go
@@ -4,8 +4,12 @@ import (
 	"DZ_7/internal/model"
 	"DZ_7/internal/repository"
 	"DZ_7/internal/service"
+	"context"
 	"fmt"
+	"os"
+	"os/signal"
 	"sync"
+	"syscall"
 	"time"
 )
 
@@ -13,17 +17,25 @@ func main() {
 	var wg sync.WaitGroup
 
 	ch := make(chan model.TransInterface)
+
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
 	go func() {
 		defer wg.Done()
-		count := 0
 		for {
-			trans := service.Create()
-			ch <- trans
-			time.Sleep(80 * time.Millisecond)
-			count++
-			if count > 100 { //8 sec
+			select {
+			case <-ctx.Done():
 				close(ch)
-				break
+				fmt.Println("cancel1")
+				return
+			case <-time.After(80 * time.Millisecond):
+				trans := service.Create()
+				fmt.Println("+create: ", trans.GetName(), trans.GetType())
+				ch <- trans
 			}
 		}
 	}()
@@ -38,6 +50,7 @@ func main() {
 				return
 			}
 			repository.Insert(result)
+			//fmt.Println("insert: ", result.GetName())
 		}
 	}()
 	wg.Add(1)
@@ -47,25 +60,38 @@ func main() {
 		countCar := 0
 		countBoat := 0
 		for {
-			time.Sleep(200 * time.Millisecond)
-			tmp := repository.GetCount(model.AirplaneType)
-			for ; countAir < tmp; countAir++ {
-				fmt.Println("air: ", repository.GetAir(countAir).Name)
-			}
+			select {
+			case <-ctx.Done():
+				fmt.Println("cancel2")
+				return
+			case <-time.After(200 * time.Millisecond):
+				//time.Sleep(200 * time.Millisecond)
+				tmp := repository.GetCount(model.AirplaneType)
+				for ; countAir < tmp; countAir++ {
+					fmt.Println("-air: ", repository.GetAir(countAir).Name)
+				}
 
-			tmp = repository.GetCount(model.CarType)
-			for ; countCar < tmp; countCar++ {
-				fmt.Println("car: ", repository.GetCar(countCar).Name)
-			}
+				tmp = repository.GetCount(model.CarType)
+				for ; countCar < tmp; countCar++ {
+					fmt.Println("-car: ", repository.GetCar(countCar).Name)
+				}
 
-			tmp = repository.GetCount(model.BoatType)
-			for ; countBoat < tmp; countBoat++ {
-				fmt.Println("boat: ", repository.GetBoat(countBoat).Name)
+				tmp = repository.GetCount(model.BoatType)
+				for ; countBoat < tmp; countBoat++ {
+					fmt.Println("-boat: ", repository.GetBoat(countBoat).Name)
+				}
 			}
 		}
 	}()
 
 	fmt.Println("wait...")
+
+	sig := <-sigs
+	if sig == syscall.SIGINT {
+		fmt.Println("ctrl-c")
+	}
+
+	cancel()
 	wg.Wait()
 
 	fmt.Println("rez:")
